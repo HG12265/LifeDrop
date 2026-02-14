8ea1d2c1e40245a31c00c10e1d5652bc12b6d730
HG12265 <gowtham114411@gmail.com>
2026-02-14 21:15:51 +0530

    running

--- backend/app.py
+++ backend/app.py
@@ -1463,4 +1463,5 @@ def force_inventory_init():
 
 # Intha block-ah ippadiye vidunga
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
